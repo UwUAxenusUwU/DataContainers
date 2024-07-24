1b125dda4956de71b8a9aa3dfd0a82f26a9e7edb
Axenus <platon-gamer@mail.ru>
2024-07-25 00:49:51 +0800

    Tree working

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,51 @@
+﻿//#define TREE_BASE_CHECK
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(0);
+#if TREE_BASE_CHECK
+            Console.WriteLine("Set Tree size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                //Tree tree = new Tree();
+                //for (int i = 0; i < n; i++)
+                //{
+                //    tree.Insert(rnd.Next(100));
+                //}
+                //tree.Print();
+                //Console.WriteLine(tree.Min());
+                //Console.WriteLine(tree.Max());
+                //Console.WriteLine(tree.Count());
+                //Console.WriteLine(tree.Sum());
+                //Console.WriteLine(tree.Avg());
+
+                UniqTree tree1 = new UniqTree();
+                for (int i = 0; i < n; i++)
+                {
+                    tree1.Insert(rnd.Next(100));
+                }
+                tree1.Print();
+                Console.WriteLine(tree1.Min());
+                Console.WriteLine(tree1.Max());
+                Console.WriteLine(tree1.Count());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            } 
+#endif
+            Tree tree = new Tree() { 3,5,8,13,21};
+        }
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class Tree
+    {
+        public class Element
+        {
+            public int Data;
+            public Element pLeft;
+            public Element pRight;
+            public Element(int Data, Element pLeft=null, Element pRight=null)
+            {
+                this.Data = Data;
+                this.pLeft = pLeft;
+                this.pRight = pRight;
+                //Console.WriteLine($"Ecnstr:\t{GetHashCode()}");
+            }
+            ~Element()
+            {
+                //Console.WriteLine($"Edstr:\t{GetHashCode()}");
+            }
+        }
+        public Element Root;
+        public Tree()
+        {
+            //Console.WriteLine($"Tcnstr:\t{GetHashCode()}");
+        }
+        ~Tree()
+        {
+            //Console.WriteLine($"Tdstr:\t{GetHashCode()}");
+        }
+
+        public void Insert(int Data)
+        {
+            Insert(Data, Root);
+        }
+        void Insert(int Data, Element Root)
+        {
+            if (this.Root == null ) { this.Root = new Element(Data); }
+            if (Root == null) return;
+            if (Data < Root.Data)
+            {
+                if(Root.pLeft == null) 
+                {
+                    Root.pLeft = new Element(Data);
+                }
+                else Insert(Data, Root.pLeft);
+            }
+            else
+            {
+                if (Root.pRight == null)
+                    {
+                    Root.pRight = new Element(Data);
+                    }
+                else Insert(Data,Root.pRight);
+            }
+        }
+        public int Min()
+        {
+            if (Root == null) throw new Exception("No min in Tree");
+            return Min(Root);
+        }
+        int Min(Element Root)
+        {
+            if (Root.pLeft == null) return Root.Data;
+            else return Min(Root.pLeft);
+        }
+        public int Max()
+        {
+            if (Root == null) throw new Exception("No max in Tree");
+            return Max(Root);
+        }
+        int Max(Element Root)
+        {
+            if (Root.pRight == null) return Root.Data;
+            else return Max(Root.pRight);
+        }
+        public int Count()
+        {
+            return Count(Root);
+        }
+        int Count(Element Root)
+        {
+            return Root==null? 0 : Count(Root.pLeft) + Count(Root.pRight) + 1;
+        }
+        public int Sum()
+        {
+            return Sum(Root);
+        }
+        int Sum(Element Root)
+        {
+            return Root == null ? 0 : Sum(Root.pLeft)+Sum(Root.pRight)+Root.Data;
+        }
+        public double Avg()
+        {
+            return (double)Sum(Root)/Count(Root);
+        }
+        public void Print()
+        {
+            Print(Root);
+            Console.WriteLine();
+        }
+        void Print(Element Root)
+        {
+            if (Root == null) return;
+            Print(Root.pLeft);
+            Console.WriteLine($"{Root.Data}\t");
+            Print(Root.pRight);
+        }
+    }
+}
